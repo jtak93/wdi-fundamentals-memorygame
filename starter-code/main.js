8d055710e666627f4c465305e2cf1867a549636e
James Tak <Tak@Jamess-MacBook-Pro.local>
2016-08-12 00:35:23 -0700

    fixed game navigation

--- starter-code/main.js
+++ starter-code/main.js
@@ -41,52 +41,9 @@ function isTwoCards(){
     cardsInPlay = [];
   }
 }
+var resetButton = document.createElement("BUTTON");
+document.body.appendChild(resetButton);
 
+//function resetBoard
 //runs board creation
 createBoard();
-
-/*var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour= "king";*/
-
-/*if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else{
-	alert("Sorry, try again");
-}
-
-if (cardOne === cardThree) {
-	alert("You found a match!");
-} else{
-	alert("Sorry, try again");
-}
-
-if (cardOne === cardFour) {
-	alert("You found a match!");
-} else{
-	alert("Sorry, try again");
-}
-
-if (cardTwo === cardThree) {
-	alert("You found a match!");
-} else{
-	alert("Sorry, try again");
-}
-
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-} else{
-	alert("Sorry, try again");
-}
-
-if (cardThree === cardFour) {
-	alert("You found a match!");
-} else{
-	alert("Sorry, try again");
-}
-
-Commented out for later use*/
-
-
-
