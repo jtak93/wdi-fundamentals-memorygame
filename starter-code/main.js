cce36f5cb004b40b7bb3a0412098e21facea4cad
James Tak <Tak@Jamess-MacBook-Pro.local>
2016-08-11 17:07:54 -0700

    Used DOM to add and modify some HTML elements. Need to fix and clean code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,25 @@
+var gameBoard = document.getElementById('game-board');
+var cardCount = 4;
+/*for (var i=1; i<=cardCount; i++){
+	var newDivItem = document.createElement('div');
+	newDivItem.className = 'card';
+	document.getElementById('game-board').appendChild(newDivItem);
+};*///function also creates cards?
+
+var createBoard = function(){
+	for (var i=0; i<4; i++){
+		var newDivItem = document.createElement('div');
+		newDivItem.className = 'card';
+		document.getElementById('game-board').appendChild(newDivItem);
+	};
+}
+createBoard();
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour= "king";
 
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else{
 	alert("Sorry, try again");
@@ -38,6 +54,8 @@ if (cardThree === cardFour) {
 } else{
 	alert("Sorry, try again");
 }
+
+Commented out for later use*/
 
 
 
